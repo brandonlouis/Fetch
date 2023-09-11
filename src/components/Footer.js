6c4dd3c58f8fa89eb35868bc0823c9339c1cb2f7
brandonlouis <44594625+brandonlouis@users.noreply.github.com>
2023-09-11 13:47:29 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <>
             <Box sx={{color:'gray', fontSize:'12px', textAlign:'center', paddingBottom:'10px'}}>
-                Coded and designed by <a style={{color: 'dimgray'}} href="https://github.com/brandonlouis?tab=repositories">Brandon Louis Chia</a>.
+                Coded and designed by <a style={{color: 'dimgray'}} href="https://github.com/brandonlouis">Brandon Louis Chia</a>.
             </Box>
         </>
     )
